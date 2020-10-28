e94a48912be363d1e86e03f610e14eaa5d81cc34
TamMelPer <mystic101@gmail.com>
2020-10-28 16:44:43 +0000

    Completed Question 10

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,3 +1,10 @@
 class DockingStation
+  def release_bike
+    @bike = Bike.new
+  end
+end
 
+class Bike
+  def working?
+  end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,5 +1,19 @@
 require 'boris_bikes'
 
 describe DockingStation do
+  it {is_expected.to respond_to(:release_bike)}
+# i want docking_station.release_bike to create a new object Bike
+# i want the bike to be working = true
+#  subject.release_bike {is_expected.to respond_to(:working?)}
+  it "is expected to respond to working?" do
+    expect(subject.release_bike).to respond_to(:working?)
+    end
 
+    it "is expected to get a bike" do
+      expect(subject.release_bike).not_to eq nil
+    end
+end
+
+describe Bike do
+  it {is_expected.to respond_to(:working?)}
 end
