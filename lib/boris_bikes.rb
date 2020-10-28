75e24680f6c08d7a887da9c932eddf2bc308b5be
TamMelPer <mystic101@gmail.com>
2020-10-28 18:12:04 +0000

    adding exercise 11

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -2,9 +2,18 @@ class DockingStation
   def release_bike
     @bike = Bike.new
   end
+
+  def dock_bike(bike)
+    @bike = bike
+  end
+
+  def see_docked_bike
+    @bike
+  end
 end
 
 class Bike
   def working?
+    true
   end
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -12,6 +12,18 @@ describe DockingStation do
     it "is expected to get a bike" do
       expect(subject.release_bike).not_to eq nil
     end
+
+    describe "#dock_bike" do
+      it "docks the bike at the docking station" do
+        expect(subject).to respond_to(:dock_bike)
+      end
+    end
+
+  describe "#see_docked_bike" do
+    it "Enables the user to see the docked bike" do
+      expect(subject).to respond_to(:see_docked_bike)
+    end
+  end
 end
 
 describe Bike do
