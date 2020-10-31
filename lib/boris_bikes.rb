cbf7093b5b498e9e713a6260850ab64f3617028a
TamMelPer <mystic101@gmail.com>
2020-10-31 10:50:14 +0000

    Step 16

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,19 +1,30 @@
 class DockingStation
-attr_reader :bikes
+#attr_reader :bikes
 # attr_reader :bikes (just add an s)
+DEFAULT_CAPACITY = 20
 
 def initialize
   @bikes = []
 end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock_bike(bike)
-    fail 'Docking Station Full' if @bikes.count >= 20
+    fail 'Docking Station Full' if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -20,7 +20,9 @@ describe DockingStation do
 
     describe "#dock_bike" do
       it "raises an error when the station is full" do
-        20.times { subject.dock_bike Bike.new }
+        DockingStation::DEFAULT_CAPACITY.times do
+          subject.dock_bike Bike.new
+        end
         expect { subject.dock_bike Bike.new }.to raise_error 'Docking Station Full'
       end
     end
